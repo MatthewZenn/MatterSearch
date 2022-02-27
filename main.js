3d98860c18352be4348353f410e7c7e504f06589
Matthew Zenn <spyguy57@gmail.com>
2022-02-27 18:22:56 -0500

    Updated Icons

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
-document.getElementById("search").addEventListener("click", Search);
-
-function Search() {
-    var name = document.getElementById("name").value
+document.getElementById("search").addEventListener("click", () => {
+    var firstname = document.getElementById("firstname").value;
+    var surname = document.getElementById("name").value;
     var place = document.getElementById("place").value;
     var date = document.getElementById("date").value;
     var modify = document.getElementById("modify").value;
-    if (name == null || name == "") {
-        return false
+
+    if (surname == null || surname == "") {
+        return false;
     }
     else {
-        chrome.tabs.create({"url": "https://www.google.com/search?q="+name+" "+place+" "+date+" "+modify})
+        chrome.tabs.create({"url": "https://www.google.com/search?q="+firstname+surname+" "+place+" "+date+" "+modify})
     }
-}
+});
 
 document.getElementById("filter1").addEventListener("click", () => {
-    var name = document.getElementById("name").value
+    var firstname = document.getElementById("firstname").value;
+    var surname = document.getElementById("name").value;
     var place = document.getElementById("place").value;
     var date = document.getElementById("date").value;
-    chrome.tabs.create({"url": "https://www.familysearch.org/search/discovery/results?surname="+name+"&birthPlace="+place+"&birthDate="+date})
+    if (surname == null || surname == "") {
+        chrome.tabs.create({"url": "https://www.familysearch.org"});
+    }
+    else {
+        chrome.tabs.create({"url": "https://www.familysearch.org/search/discovery/results?givenName="+firstname+"&surname="+surname+"&birthPlace="+place+"&birthDate="+date});
+    }
 });
 
 document.getElementById("filter2").addEventListener("click", () => {
-    chrome.tabs.create({"url": "https://www.ancestory.com"})
+    chrome.tabs.create({"url": "https://www.ancestory.com"});
 });
 
 document.getElementById("filter3").addEventListener("click", () => {
-    chrome.tabs.create({"url": "https://www.census.gov"})
+    chrome.tabs.create({"url": "https://www.census.gov"});
 });
